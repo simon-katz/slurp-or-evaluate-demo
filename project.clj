b7278b23d0d3ce99af14f8deeda231e4773ae3d4
Simon Katz <sk@nomistech.com>
2017-03-19 12:46:51 +0000

    Upgrade to slurp-or-evaluate 0.1.3

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.nomistech/slurp-or-evaluate "0.1.2"]])
+                 [com.nomistech/slurp-or-evaluate "0.1.3"]])
